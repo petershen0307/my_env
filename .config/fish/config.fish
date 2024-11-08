d77ebc1b952bd0b4a4e0b0bc18639e4de5b9a9ff
Peter Shen <petershen0307@hotmail.com>
2024-11-08 14:38:12 +0800

    Create config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,35 @@
+source ~/.asdf/asdf.fish
+
+if not test -e ~/.config/fish/completions
+    mkdir -p ~/.config/fish/completions; and ln -s ~/.asdf/completions/asdf.fish ~/.config/fish/completions
+end
+
+# function
+function pbcopy
+    xclip -selection clipboard
+end
+
+function pbpaste
+    xclip -selection clipboard -o
+end
+
+function chd
+    cd "$(find $HOME/code -type d -not -path "*/.git/*" | fzf)"
+end
+
+function pgf
+    set -f branch $argv[1]
+    if test -z $branch
+        set branch main
+    end
+    git fetch origin --prune
+    git fetch . origin/$branch:$branch
+end
+
+function code
+    "/mnt/c/Users/peter_shen/AppData/Local/Programs/Microsoft VS Code/bin/code" $argv
+end
+
+function code-insiders
+    "/mnt/c/Users/peter/AppData/Local/Programs/Microsoft VS Code Insiders/bin/code-insiders" $argv
+end
