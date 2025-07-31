c74dbf69c39157037458baeee5578c8d152b771c
Peter Shen <petershen0307@hotmail.com>
2025-07-31 21:47:08 +0800

    Update chd.fish

--- .config/fish/functions/chd.fish
+++ .config/fish/functions/chd.fish
@@ -1,6 +1,6 @@
 # change to project directory
 function chd
-    set dir (find $HOME/wsl_code -type d -not -path "*/.git/*" | fzf)
+    set dir (find $HOME/wsl_code -maxdepth 4 -type d -not -path "*/.git/*" | fzf)
     if test -n "$dir"
         cd $dir
     else
